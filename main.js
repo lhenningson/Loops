8cfe5f610d6f953a76e727cf328f2161c5f0c1af
lhenningson <l.henningson@share.epsb.ca>
2022-11-18 10:20:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ let numheads = 0;
 let numtails = 0;
 let numflips = 0;
 
-while (numheads >) {
-    console.log(num);
-    num--;
+while (numheads < 100) {
+    if (Math.random() < 0.5) {
+        numheads++;
+    } else {
+        numtails++;
+    }
+    numflips++;
 }
 
-console.log("Blast Off!")
+console.log("Number of Coin Flips: " + numflips);
+console.log("Heads: " + numheads + " Tails: " + numtails);
