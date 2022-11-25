06ad2961f03ed8b54be36592f73b02789cf21558
lhenningson <l.henningson@share.epsb.ca>
2022-11-25 11:21:21 -0700

    jdkslage

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 600;
+cnv.width = 400;
 cnv.height = 400;
 
-let htmlimg = document.getElementById("html-logo")
-ctx.drawImage(htmlimg, 50, 50, 300, 300);
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 400, 300);
+
+ctx.fillStyle = "red";
+ctx.beginPath();
+ctx.arc(200, 300, 15, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.fillStyle = "green";
+ctx.fillRect(0, 300, 400, 100);
+
+let img1 = document.getElementById("html-cloud");
+ctx.drawImage(img1, 150, 100);
+
